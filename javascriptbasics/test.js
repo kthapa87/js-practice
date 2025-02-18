376597bc745d6f1453b9c18f27f49a1bc40d843b
kthapa87 <kamalt2k@gmail.com>
2025-02-18 21:32:26 +0000

    Math.methods completed

--- javascriptbasics/test.js
+++ javascriptbasics/test.js
@@ -1,2 +1,41 @@
-console.log('hallo this is javascript');
-console.log('hallo');
+// // console.log('hallo this is javascript');
+// // console.log('hallo');
+// // const score = 400
+// // console.log(score);
+
+// // const balance = new Number(100)
+// // console.log(balance);
+
+// // console.log(balance.toString().length);
+// // console.log(balance.toFixed(0));
+
+// // const othernum = 23.8966
+
+// // console.log(othernum.toPrecision(5))
+
+// // const hun = 100000000
+
+// // console.log(hun.toLocaleString('en-IN'))
+
+
+// // maths
+
+// console.log(Math.round(4.6)) // round of the given number
+
+// console.log(Math.abs(-4)) // give absolute value without sign
+// console.log(Math.ceil(4.1)) // give max number e.g 4.1 becomes 5 more than 4 i.e 5
+// console.log(Math.floor(4.8)) // give min number withour rounding e.g 4.8 becomes 4 min value
+// console.log(Math.min(4,5,9,1)) // give min number i.e 1 in our case
+// console.log(Math.max(4,5,9,1))// give greater number i.e 9 in our case
+// console.log(Math.random())// values always between 0 and 1
+
+// const ran = Math.random()*100000
+// console.log(Math.round(ran))
+
+ const min = 10,max=20;
+const ran =Math.random()
+console.log(ran)
+console.log(ran*(max-min + 1) + min)
+
+
+
